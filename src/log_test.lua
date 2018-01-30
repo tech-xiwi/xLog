e833d387841459c1c51d7ee33cdb65aa5c7a8d29
tech-xiwi <xiwi92@hotmail.com>
2018-01-30 16:45:40 +0800

    first commit

---
+++ src/log_test.lua
@@ -0,0 +1,24 @@
+local log = require("xlog")
+local socket = require "socket"
+local t0 = socket.gettime()
+
+tag = "log_test.lua"
+
+for i=1,100000 do
+    -- log.v(tag,"test long content "..i)
+    if i % 5 == 0 then
+        log.i(tag,"test long content "..i)
+    elseif i % 5 == 1 then
+        log.v(tag,"test long content "..i)
+    elseif i % 5 == 2 then
+        log.d(tag,"test long content "..i)
+    elseif i % 5 == 3 then
+        log.w(tag,"test long content "..i)
+    elseif i % 5 == 4 then
+        log.e(tag,"test long content "..i)
+    end
+end
+
+-- do something
+local t1 = socket.gettime()
+print("used time: "..t1-t0.."ms")
---
+++ src/xlog.lua
@@ -0,0 +1,76 @@
+--[==[
+
+xiwi module for Lua 5.3
+
+Version 1.0
+
+You can contact the author by sending an e-mail to 'xiwi' at the
+email 'xiwi92@hotmail.com'.
+
+
+Copyright (C) 2017-2020 xiwi
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+--]==]
+
+local xlog = {_version="1.0"}
+
+DEBUG = true
+
+function xlog.d(tag, msg)
+    if DEBUG then
+        print(log("d", tag, msg))
+    end
+end
+
+function xlog.i(tag, msg)
+    if DEBUG then
+        print(log("i", tag, msg))
+    end
+end
+
+function xlog.e(tag, msg)
+    print(log("e", tag, msg))
+end
+
+function xlog.w(tag, msg)
+    if DEBUG then
+        print(log("w", tag, msg))
+    end
+end
+
+function xlog.v(tag, msg)
+    if DEBUG then
+        print(log("v", tag, msg))
+    end
+end
+
+function log(level, tag, msg)
+    local date = os.time()
+    local millSec = string.format("%.6f", os.clock())
+    date = os.date("%Y-%m-%d %H:%M:%S", date)
+    return date .. string.sub(millSec,2,#millSec) .. " xLog : ".. level .. " : " .. tag .. " :" .. msg
+end
+
+return xlog
+
+
